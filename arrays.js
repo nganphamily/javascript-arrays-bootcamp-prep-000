5835fb110814f505e4d11d826406d0dc1809a3be
NGAN PHAM <nganvpham@gmail.com>
2018-01-25 23:49:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,5 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
 
 function addElementToBeginningOfArray(array, element){
-  var array = array;
-  return array[...element];
+  
 }
